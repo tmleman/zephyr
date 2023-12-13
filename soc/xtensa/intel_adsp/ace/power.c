1c2243240c94794381ba87342d0dda473d8022af
Tomasz Leman <tomasz.m.leman@intel.com>
2023-12-13 20:39:16 +0100

    intel_adsp: ace: power: prevent hst domain power gating
    This commit introduces a mechanism to prevent the power gating of the
    HST (Host) power domain when the primary core of the audio DSP is active.
    The power gating prevention is crucial for maintaining the functionality
    of the HST domain while the primary DSP core is performing critical tasks.
    
    Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

--- soc/xtensa/intel_adsp/ace/power.c
+++ soc/xtensa/intel_adsp/ace/power.c
@@ -5,6 +5,7 @@
  */
 #include <zephyr/kernel.h>
 #include <zephyr/pm/pm.h>
+#include <zephyr/pm/device_runtime.h>
 #include <zephyr/device.h>
 #include <zephyr/debug/sparse.h>
 #include <zephyr/cache.h>
@@ -235,6 +236,8 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	ARG_UNUSED(substate_id);
 	uint32_t cpu = arch_proc_id();
+	int ret = 0;
+	ARG_UNUSED(ret);
 
 	/* save interrupt state and turn off all interrupts */
 	core_desc[cpu].intenable = XTENSA_RSR("INTENABLE");
@@ -297,6 +300,8 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 			hpsram_mask = (1 << ebb_banks) - 1;
 #endif /* CONFIG_ADSP_POWER_DOWN_HPSRAM */
 			/* do power down - this function won't return */
+			ret = pm_device_runtime_put(INTEL_ADSP_HST_DOMAIN_DEV);
+			__ASSERT(ret == 0, "HST power domain suspend operation failed.");
 			power_down(true, uncache_to_cache(&hpsram_mask),
 				   true);
 		} else {
@@ -312,6 +317,9 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 			battr |= (DSPBR_BATTR_LPSCTL_RESTORE_BOOT & LPSCTL_BATTR_MASK);
 			DSPCS.bootctl[cpu].battr = battr;
 		}
+
+		ret = pm_device_runtime_put(INTEL_ADSP_HST_DOMAIN_DEV);
+		__ASSERT(ret == 0, "HST power domain suspend operation failed.");
 		power_gate_entry(cpu);
 	} else {
 		__ASSERT(false, "invalid argument - unsupported power state");
@@ -323,6 +331,12 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 {
 	ARG_UNUSED(substate_id);
 	uint32_t cpu = arch_proc_id();
+
+	if (cpu == 0) {
+		int ret = pm_device_runtime_get(INTEL_ADSP_HST_DOMAIN_DEV);
+		ARG_UNUSED(ret);
+		__ASSERT(ret == 0, "HST power domain resume operation failed.");
+	}
 
 	if (state == PM_STATE_SOFT_OFF) {
 		/* restore clock gating state */
